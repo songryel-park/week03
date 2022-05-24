fdeb4c4daf4a670f2baff16ca96101add9d34db3
songryel-park <fuf3413@gmail.com>
2022-05-24 17:00:46 +0900

    controller

---
+++ src/main/java/com/sparta/week3/controller/CommentController.java
@@ -0,0 +1,34 @@
+package com.sparta.week3.controller;
+
+import com.sparta.week3.domain.Comment;
+import com.sparta.week3.domain.CommentRepository;
+import com.sparta.week3.domain.CommentRequestDto;
+import com.sparta.week3.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class CommentController {
+    private final CommentRepository commentRepository;
+    private final CommentService commentService;
+
+    @PostMapping("/api/comments")
+    public Comment createComment(@RequestBody CommentRequestDto requestDto) {
+        Comment ment = new Comment(requestDto);
+        return commentRepository.save(ment);
+    }
+
+    @GetMapping("/api/comments")
+    public List<Comment> getMemos() {
+        return commentRepository.findAllByOrderByModifiedAtDesc();
+    }
+
+    @DeleteMapping("/api/comments/{id}")
+    public Long deleteComment(@PathVariable Long id) {
+        commentRepository.deleteById(id);
+        return id;
+    }
+}
