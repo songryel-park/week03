8cd6d4d625fabc079150b88b4f4f95f0529e34ee
songryel-park <fuf3413@gmail.com>
2022-05-24 16:19:50 +0900

    no message

---
+++ src/main/java/com/sparta/week3/domain/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.sparta.week3.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Memo> findAllByOrderByModifiedAtDesc();
+}
