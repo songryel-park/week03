53d0aa0feebd86da19621b45e55ed25e919f2d88
songryel-park <fuf3413@gmail.com>
2022-05-24 17:32:01 +0900

    .

--- src/main/java/com/sparta/week3/domain/Comment.java
+++ src/main/java/com/sparta/week3/domain/Comment.java
@@ -5,10 +5,10 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@NoArgsConstructor // 기본생성자를 만듭니다.
+@NoArgsConstructor
 @Getter
-@Entity // 테이블과 연계됨을 스프링에게 알려줍니다.
-public class Comment extends Timestamped { // 생성,수정 시간을 자동으로 만들어줍니다.
+@Entity
+public class Comment extends Timestamped {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private Long id;
