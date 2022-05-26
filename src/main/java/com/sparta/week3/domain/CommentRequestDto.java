091e4fd7acedb427d27bbdcb76f4d9c55fd378c2
songryel-park <fuf3413@gmail.com>
2022-05-26 18:12:59 +0900

    수정본

--- src/main/java/com/sparta/week3/domain/CommentRequestDto.java
+++ src/main/java/com/sparta/week3/domain/CommentRequestDto.java
@@ -1,12 +1,8 @@
 package com.sparta.week3.domain;
 
 import lombok.Getter;
-import lombok.Setter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@Setter
-@RequiredArgsConstructor
 public class CommentRequestDto {
     private String title;
     private String username;
